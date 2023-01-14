8e9271ef5604feb3de8bdcefbbd72a2cad8a0ddb
Erik Rasmussen <ecr.referee@hotmail.com>
2023-01-13 21:48:55 -0600

    Refactor to remove ParseResult

--- src/common/dotnet/src/Safir.Audio/Vorbis.fs
+++ src/common/dotnet/src/Safir.Audio/Vorbis.fs
@@ -14,36 +14,35 @@ let pVorbisComment (f: ReadOnlySpan<byte>) (length: int) =
     let s = Encoding.UTF8.GetString(f.Slice(0, length))
     let p = s.Split("=")
 
-    if Array.length p = 2 then
+    if Array.length p <> 2 then
+        None
+    else
         let name = p[0]
         let value = p[1]
 
-        let res =
-            match name with
-            | InvariantEqual "TITLE" -> Title value
-            | InvariantEqual "VERSION" -> VorbisComment.Version value
-            | InvariantEqual "ALBUM" -> Album value
-            | InvariantEqual "TRACKNUMBER" -> TrackNumber value
-            | InvariantEqual "ARTIST" -> Artist value
-            | InvariantEqual "PERFORMER" -> Performer value
-            | InvariantEqual "COPYRIGHT" -> Copyright value
-            | InvariantEqual "LICENSE" -> License value
-            | InvariantEqual "ORGANIZATION" -> Organization value
-            | InvariantEqual "DESCRIPTION" -> Description value
-            | InvariantEqual "GENRE" -> Genre value
-            | InvariantEqual "DATE" -> Date value
-            | InvariantEqual "LOCATION" -> Location value
-            | InvariantEqual "CONTACT" -> Contact value
-            | InvariantEqual "ISRC" -> Isrc value
-            | _ -> Other(name, value)
-
-        { Remaining = f.Slice(length)
-          Result = Some res }
-    else
-        { Remaining = f; Result = None }
+        match name with
+        | InvariantEqual "TITLE" -> Title value
+        | InvariantEqual "VERSION" -> VorbisComment.Version value
+        | InvariantEqual "ALBUM" -> Album value
+        | InvariantEqual "TRACKNUMBER" -> TrackNumber value
+        | InvariantEqual "ARTIST" -> Artist value
+        | InvariantEqual "PERFORMER" -> Performer value
+        | InvariantEqual "COPYRIGHT" -> Copyright value
+        | InvariantEqual "LICENSE" -> License value
+        | InvariantEqual "ORGANIZATION" -> Organization value
+        | InvariantEqual "DESCRIPTION" -> Description value
+        | InvariantEqual "GENRE" -> Genre value
+        | InvariantEqual "DATE" -> Date value
+        | InvariantEqual "LOCATION" -> Location value
+        | InvariantEqual "CONTACT" -> Contact value
+        | InvariantEqual "ISRC" -> Isrc value
+        | _ -> Other(name, value)
+        |> Some
 
 let pVorbisCommentHeader (f: ReadOnlySpan<byte>) (length: int) =
-    if int64 length < ((pown 2L 32) - 1L) then
+    if int64 length >= ((pown 2L 32) - 1L) then
+        None
+    else
         // Read as uint32 because that's what the spec defines,
         // but cast to an int so we can slice with it
         let vl = BinaryPrimitives.ReadUInt32LittleEndian(f.Slice(0, 4)) |> int
@@ -59,19 +58,15 @@ let pVorbisCommentHeader (f: ReadOnlySpan<byte>) (length: int) =
             let l = BinaryPrimitives.ReadUInt32LittleEndian(f.Slice(o, 4)) |> int
             let c = pVorbisComment (f.Slice(o + 4)) l
 
-            match c.Result with
+            match c with
             | Some x -> uc <- x :: uc
             | None -> ()
 
             o <- o + 4 + l
 
-        if f[o + 1] &&& 0x01uy = 0x01uy then
-            { Remaining = f.Slice(length)
-              Result =
-                { VendorString = vs
-                  UserComments = uc |> List.rev }
-                |> Some }
+        if f[o + 1] &&& 0x01uy = 0x00uy then
+            None
         else
-            { Remaining = f; Result = None }
-    else
-        { Remaining = f; Result = None }
+            { VendorString = vs
+              UserComments = uc |> List.rev }
+            |> Some
