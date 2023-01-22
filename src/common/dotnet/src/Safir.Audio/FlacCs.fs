3fdc32a13122b11c297da20ef65834852ff07347
Erik Rasmussen <ecr.referee@hotmail.com>
2023-01-22 14:08:03 -0600

    Defining higher-level API

--- src/common/dotnet/src/Safir.Audio/FlacCs.fs
+++ src/common/dotnet/src/Safir.Audio/FlacCs.fs
@@ -5,21 +5,27 @@ let ReadMagic f = Flac.readMagic f
 let ReadMetadataBlockHeader f = Flac.readMetadataBlockHeader f
 
 let ReadMetadataBlockStreamInfo f =
-    Flac.readMetadataBlockStreamInfo f |> MetadataBlockStreamInfoValueCs
+    let result = Flac.readMetadataBlockStreamInfo f
+    MetadataBlockStreamInfoValueCs(result)
 
 let ReadMetadataBlockPadding f l =
-    Flac.readMetadataBlockPadding f l |> MetadataBlockPaddingValueCs
+    let result = Flac.readMetadataBlockPadding f l
+    MetadataBlockPaddingValueCs(result)
 
 let ReadMetadataBlockApplication f l =
-    Flac.readMetadataBlockApplication f l |> MetadataBlockApplicationValueCs
+    let result = Flac.readMetadataBlockApplication f l
+    MetadataBlockApplicationValueCs(result)
 
 let ReadMetadataBlockSeekTable f l =
-    Flac.readMetadataBlockSeekTable f l |> MetadataBlockSeekTableValueCs
+    let result = Flac.readMetadataBlockSeekTable f l
+    MetadataBlockSeekTableValueCs(result)
 
 let ReadMetadataBlockVorbisComment f l =
-    Flac.readMetadataBlockVorbisComment f l |> MetadataBlockVorbisCommentValueCs
+    let result = Flac.readMetadataBlockVorbisComment f l
+    MetadataBlockVorbisCommentValueCs(result)
 
 let ReadMetadataBlockPicture f l =
-    Flac.readMetadataBlockPicture f l |> MetadataBlockPictureValueCs
+    let result = Flac.readMetadataBlockPicture f l
+    MetadataBlockPictureValueCs(result)
 
-let ReadFlacStream f = Flac.readFlacStream f |> FlacStreamCs
+// let ReadFlacStream f = Flac.readFlacStream f |> FlacStreamCs
