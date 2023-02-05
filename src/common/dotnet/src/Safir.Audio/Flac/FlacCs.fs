2776459ff70d5bcab2e24e4bb2d7272a52d85691
Erik Rasmussen <ecr.referee@hotmail.com>
2023-02-05 12:52:34 -0600

    Fix ReadStream

--- src/common/dotnet/src/Safir.Audio/Flac/FlacCs.fs
+++ src/common/dotnet/src/Safir.Audio/Flac/FlacCs.fs
@@ -1,22 +1,28 @@
 module Safir.Audio.FlacCs
 
-let ReadMagic (r: byref<FlacStreamReader>) = Flac.readMagic &r
+let ToMetadataBlockStreamInfoCs block =
+    MetadataBlockStreamInfoCs(
+        block.MinBlockSize,
+        block.MaxBlockSize,
+        block.MinFrameSize,
+        block.MaxFrameSize,
+        block.SampleRate,
+        block.Channels,
+        block.BitsPerSample,
+        block.TotalSamples,
+        block.Md5Signature
+    )
 
-let ReadMetadataBlockHeader (r: byref<FlacStreamReader>) = Flac.readMetadataBlockHeader &r
+let ToMetadataBlockPaddingCs block =
+    let (MetadataBlockPadding length) = block
+    MetadataBlockPaddingCs(length)
 
-let ReadMetadataBlockStreamInfo (r: byref<FlacStreamReader>) = Flac.readMetadataBlockStreamInfo &r
+let ToMetadataBlockApplicationCs block =
+    MetadataBlockApplicationCs(block.ApplicationId, block.ApplicationData)
 
-let ReadMetadataBlockPadding (r: byref<FlacStreamReader>) =
-    let (MetadataBlockPadding length) = Flac.readMetadataBlockPadding &r
-    { Length = length }
-
-let ReadMetadataBlockApplication (r: byref<FlacStreamReader>) = Flac.readMetadataBlockApplication &r
-
-let ReadMetadataBlockSeekTable (r: byref<FlacStreamReader>) =
-    let result = Flac.readMetadataBlockSeekTable &r
-
-    { Points =
-        result.Points
+let ToMetadataBlockSeekTableCs block =
+    let points =
+        block.Points
         |> List.map (function
             | Placeholder ->
                 { IsPlaceHolder = true
@@ -28,14 +34,91 @@ let ReadMetadataBlockSeekTable (r: byref<FlacStreamReader>) =
                   FrameSamples = x.FrameSamples
                   SampleNumber = x.SampleNumber
                   StreamOffset = x.StreamOffset })
-        |> List.toSeq }
+        |> List.toSeq
+
+    MetadataBlockSeekTableCs(points)
+
+let ToMetadataBlockVorbisCommentCs block =
+    let (MetadataBlockVorbisComment result) = block
+    let comments = result.UserComments |> List.map VorbisCs.toComment |> List.toSeq
+    MetadataBlockVorbisCommentCs(result.Vendor, comments)
+
+let ToCueSheetTrackCs track =
+    let isrc =
+        match track.Isrc with
+        | Some x -> x
+        | None -> null
+
+    CueSheetTrackCs(track.Offset, track.Number, isrc, track.IsAudio, track.PreEmphasis, track.IndexPoints)
+
+let ToMetadataBlockCueSheetCs block =
+    MetadataBlockCueSheetCs(
+        block.CatalogNumber,
+        block.LeadInSamples,
+        block.IsCompactDisc,
+        block.TotalTracks,
+        block.Tracks |> List.map ToCueSheetTrackCs
+    )
+
+let ToMetadataBlockPictureCs block =
+    MetadataBlockPictureCs(
+        block.Type,
+        block.MimeType,
+        block.Description,
+        block.Width,
+        block.Height,
+        block.Depth,
+        block.Colors,
+        block.DataLength,
+        block.Data
+    )
+
+let ToMetadataBlockDataCs =
+    function
+    | StreamInfo x -> ToMetadataBlockStreamInfoCs x :> MetadataBlockDataCs
+    | Padding x -> ToMetadataBlockPaddingCs x
+    | Application x -> ToMetadataBlockApplicationCs x
+    | SeekTable x -> ToMetadataBlockSeekTableCs x
+    | VorbisComment x -> ToMetadataBlockVorbisCommentCs x
+    | CueSheet x -> ToMetadataBlockCueSheetCs x
+    | Picture x -> ToMetadataBlockPictureCs x
+    | Skipped -> readerEx "TODO"
+
+let ToMetadataBlockCs block =
+    MetadataBlockCs(block.Header, block.Data |> ToMetadataBlockDataCs)
+
+let ToFlacStreamCs stream =
+    FlacStreamCs(stream.Metadata |> List.map ToMetadataBlockCs)
+
+let ReadMagic (r: byref<FlacStreamReader>) = Flac.readMagic &r
+
+let ReadMetadataBlockHeader (r: byref<FlacStreamReader>) = Flac.readMetadataBlockHeader &r
+
+let ReadMetadataBlockStreamInfo (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockStreamInfo &r |> ToMetadataBlockStreamInfoCs
+
+let ReadMetadataBlockPadding (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockPadding &r |> ToMetadataBlockPaddingCs
+
+let ReadMetadataBlockApplication (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockApplication &r |> ToMetadataBlockApplicationCs
+
+let ReadMetadataBlockSeekTable (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockSeekTable &r |> ToMetadataBlockSeekTableCs
 
 let ReadMetadataBlockVorbisComment (r: byref<FlacStreamReader>) =
-    let (MetadataBlockVorbisComment result) = Flac.readMetadataBlockVorbisComment &r
+    Flac.readMetadataBlockVorbisComment &r |> ToMetadataBlockVorbisCommentCs
 
-    { Vendor = result.Vendor
-      UserComments = result.UserComments |> List.map VorbisCs.toComment |> List.toSeq }
+let ReadMetadataBlockCueSheet (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockCueSheet &r |> ToMetadataBlockCueSheetCs
 
-let ReadMetadataBlockPicture (r: byref<FlacStreamReader>) = Flac.readMetadataBlockPicture &r
+let ReadMetadataBlockPicture (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlockPicture &r |> ToMetadataBlockPictureCs
 
-// let ReadFlacStream f = Flac.readFlacStream f |> FlacStreamCs
+let ReadMetadataBlockData (r: byref<FlacStreamReader>) (blockType: BlockType) =
+    Flac.readMetadataBlockData &r blockType |> ToMetadataBlockDataCs
+
+let ReadMetadataBlock (r: byref<FlacStreamReader>) =
+    Flac.readMetadataBlock &r |> ToMetadataBlockCs
+
+let ReadStream (r: byref<FlacStreamReader>) = Flac.readStream &r |> ToFlacStreamCs
--- src/common/dotnet/src/Safir.Audio/Flac/FlacCsTypes.fs
+++ src/common/dotnet/src/Safir.Audio/Flac/FlacCsTypes.fs
@@ -1,32 +1,114 @@
 namespace Safir.Audio
 
-type MetadataBlockPaddingCs = { Length: int }
-
 type SeekPointCs =
     { IsPlaceHolder: bool
       SampleNumber: int64
       StreamOffset: int64
       FrameSamples: int }
 
-type MetadataBlockSeekTableCs = { Points: SeekPointCs seq }
+type CueSheetTrackCs
+    (
+        offset: int64,
+        number: int16,
+        isrc: string,
+        isAudio: bool,
+        preEmphasis: bool,
+        indexPoints: CueSheetTrackIndex seq
+    ) =
+    member this.Offset = offset
+    member this.Number = number
+    member this.Isrc = isrc
+    member this.IsAudio = isAudio
+    member this.PreEmphasis = preEmphasis
+    member this.IndexPoints = indexPoints
 
-type MetadataBlockVorbisCommentCs =
-    { Vendor: string
-      UserComments: VorbisCommentCs seq }
+[<AbstractClass>]
+type MetadataBlockDataCs() =
+    class
+    end
 
-// type MetadataBlockCs(block: MetadataBlock) =
-//     member this.Header = block.Header
-//
-//     member this.Data =
-//         match block.Data with
-//         | StreamInfo x -> MetadataBlockStreamInfoCs(x) :> MetadataBlockDataCs
-//         | Padding x -> MetadataBlockPaddingCs(x)
-//         | Application x -> MetadataBlockApplicationCs(x)
-//         | SeekTable x -> MetadataBlockSeekTableCs(x)
-//         | VorbisComment x -> MetadataBlockVorbisCommentCs(x)
-//         | CueSheet x -> MetadataBlockCueSheetCs(x)
-//         | Picture x -> MetadataBlockPictureCs(x)
-//         | Skipped x -> MetadataBlockSkippedCs(x.ToArray())
-//
-// type FlacStreamCs(stream: FlacStream) =
-//     member this.Metadata = stream.Metadata |> List.toSeq
+type MetadataBlockStreamInfoCs
+    (
+        minBlockSize: int,
+        maxBlockSize: int,
+        minFrameSize: int,
+        maxFrameSize: int,
+        sampleRate: int,
+        channels: int,
+        bitsPerSample: int,
+        totalSamples: int64,
+        md5Signature: string
+    ) =
+    inherit MetadataBlockDataCs()
+    member this.MinBlockSize = minBlockSize
+    member this.MaxBlockSize = maxBlockSize
+    member this.MinFrameSize = minFrameSize
+    member this.MaxFrameSize = maxFrameSize
+    member this.SampleRate = sampleRate
+    member this.Channels = channels
+    member this.BitsPerSample = bitsPerSample
+    member this.TotalSamples = totalSamples
+    member this.Md5Signature = md5Signature
+
+type MetadataBlockPaddingCs(length: int) =
+    inherit MetadataBlockDataCs()
+    member this.Length = length
+
+type MetadataBlockApplicationCs(applicationId: int, applicationData: byte array) =
+    inherit MetadataBlockDataCs()
+    member this.ApplicationId = applicationId
+    member this.ApplicationData = applicationData
+
+type MetadataBlockSeekTableCs(points: SeekPointCs seq) =
+    inherit MetadataBlockDataCs()
+    member this.Points = points
+
+type MetadataBlockVorbisCommentCs(vendor: string, userComments: VorbisCommentCs seq) =
+    inherit MetadataBlockDataCs()
+    member this.Vendor = vendor
+    member this.UserComments = userComments
+
+type MetadataBlockCueSheetCs
+    (
+        catalogNumber: string,
+        leadInSamples: int64,
+        isCompactDisc: bool,
+        totalTracks: int,
+        tracks: CueSheetTrackCs seq
+    ) =
+    inherit MetadataBlockDataCs()
+    member this.CatalogNumber = catalogNumber
+    member this.LeadInSamples = leadInSamples
+    member this.IsCompactDisc = isCompactDisc
+    member this.TotalTracks = totalTracks
+    member this.Tracks = tracks
+
+type MetadataBlockPictureCs
+    (
+        pictureType: PictureType,
+        mimeType: string,
+        description: string,
+        width: int,
+        height: int,
+        depth: int,
+        colors: int,
+        dataLength: int,
+        data: byte array
+    ) =
+    inherit MetadataBlockDataCs()
+    member this.Type = pictureType
+    member this.MimeType = mimeType
+    member this.Description = description
+    member this.Width = width
+    member this.Height = height
+    member this.Depth = depth
+    member this.Colors = colors
+    member this.DataLength = dataLength
+    member this.Data = data
+
+type MetadataBlockCs(header: MetadataBlockHeader, data: MetadataBlockDataCs) =
+    member this.Header = header
+    member this.Data = data
+
+type FlacStreamCs(metadata: MetadataBlockCs seq) =
+    member this.Metadata = metadata
