0f7cf7e230abccd79154794b62184d1a53626ba5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-01-25 23:21:20 -0600

    New project

---
+++ src/UnMango.Audio.Flac/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace UnMango.Audio.Flac
+
+open System.IO
+
+module Flac =
+    let read thing = id
+
+    let run () =
+        let file = File.OpenRead("")
+        let test = StreamReader(file)
+        // let test2 = test.Read
