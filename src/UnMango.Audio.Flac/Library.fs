825c32264be3f705dd0206c26527a9e151b88cac
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-01-27 00:29:44 -0600

    Fiddling

--- src/UnMango.Audio.Flac/Library.fs
+++ src/UnMango.Audio.Flac/Library.fs
@@ -1,11 +1,47 @@
 ﻿namespace UnMango.Audio.Flac
 
-open System.IO
+open System.Buffers
+open System.IO.Pipelines
+
+type BlockType =
+    | StreamInfo = 0
+    | Padding = 1
+    | Application = 2
+    | SeekTable = 3
+    | VorbisComment = 4
+    | CueSheet = 5
+    | Picture = 6
+    | Invalid = 127
+
+[<Struct>]
+type MetadataBlockHeader =
+    { LastBlock: bool
+      BlockType: BlockType
+      Length: uint }
+
+[<Struct>]
+type FlacValue = MetadataBlockHeader of MetadataBlockHeader
+
+type ParseResult = (struct (ReadOnlySequence<byte> * FlacValue))
+type Parse = ReadOnlySequence<byte> -> ParseResult
+type Handle = FlacValue -> Async<unit>
 
 module Flac =
-    let read thing = id
+    let parse (buffer: ReadOnlySequence<byte>) : ParseResult =
+        buffer,
+        MetadataBlockHeader
+            { Length = 0u
+              BlockType = BlockType.Invalid
+              LastBlock = false }
 
-    let run () =
-        let file = File.OpenRead("")
-        let test = StreamReader(file)
-        // let test2 = test.Read
+type Flac =
+    static member Read(reader: PipeReader, handle: Handle, cancellationToken) = async {
+        let mutable completed = false
+
+        while not completed do
+            let! result = reader.ReadAsync(cancellationToken) |> _.AsTask() |> Async.AwaitTask
+            let struct (buffer, value) = Flac.parse result.Buffer
+            do! handle value
+            reader.AdvanceTo(buffer.Start, buffer.End)
+            completed <- result.IsCompleted
+    }
